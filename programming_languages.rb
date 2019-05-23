a4b90c295a7012e6a0406f92eed2a7e52e64f8a4
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-23 05:30:41 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
   # your code here
+  styles = languages.keys
+  new_hash = {}
+  styles.each do |style|
+
+    languages[style].each do |lang, lang_info|
+      new_hash[lang] = lang_info
+      new_hash[lang][:style] += [style]
+    end
+  end
+  puts new_hash.inspect
 end
